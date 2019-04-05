016b554a85ad469ab2acafe2080549bc058ef49a
Dan Hughes <danhughes@monzo.com>
2019-04-05 19:29:36 +0100

    Updates

---
+++ precache-manifest.7066a89196236db438550124b4e72237.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4814ef54f3c88cb980c3",
+    "url": "/awards/static/css/main.1047850a.chunk.css"
+  },
+  {
+    "revision": "4814ef54f3c88cb980c3",
+    "url": "/awards/static/js/main.e6924914.chunk.js"
+  },
+  {
+    "revision": "ad37043b52f69f830730",
+    "url": "/awards/static/js/runtime~main.7aa596e8.js"
+  },
+  {
+    "revision": "4e05ce54ef8d8aaca66e",
+    "url": "/awards/static/js/2.b5ec8439.chunk.js"
+  },
+  {
+    "revision": "378e82bd5c041f9a7e545c2a59c607fc",
+    "url": "/awards/index.html"
+  }
+];
